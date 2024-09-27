5035d0b216bcb065c87f34b678b7510779b3a65c
Artem <artem.a.baranov@yandex.ru>
2024-09-27 23:40:51 +0300

    практика 3

---
+++ assets/colors.dart
@@ -0,0 +1,19 @@
+// colors.dart
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color mainBlack = Color(0xFF212121);
+  static const Color lightestGray = Color(0xFFE6E8EB);
+  static const Color lightGrayMine = Color(0xFFD4D4D4);
+  static const Color mainBackgroundGray = Color(0xFFFAFAFA);
+  static const Color ypLightGray = Color(0xFFE6E8EB);
+  static const Color ypBlueLight = Color(0xFF9FBBF3);
+  static const Color iconsTintColor = Color(0xFFAEAFB4);
+  static const Color clearButtonColor = Color(0xFFAEAFB4);
+  static const Color mainBlue = Color(0xFF3B9EBF);
+  static const Color darkestGray = Color(0xFF48494B);
+  static const Color ypYellow = Color(0xFFFCD452);
+  static const Color bigButtonColor = lightGrayMine;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,219 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: 'IP Finder',
+      home: Scaffold(
+        body: Container(
+          color: const Color(0xFFFAFAFA),
+          child: IPInfoLayout(),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class IPInfoLayout extends StatefulWidget {
+  const IPInfoLayout({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _IPInfoLayoutState createState() => _IPInfoLayoutState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _IPInfoLayoutState extends State<IPInfoLayout> {
+  String _currentFlag = 'assets/images/flag.png';
+  String _countryName = 'Russian Federation';
+  String _cityName = "Moscow";
+  String _ipAddr = "192.168.1.1";
+  String _provider = "MGTS";
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void _updateCountryInfo() {
+    if(_countryName == 'Russian Federation'){
+      setState(() {
+        _currentFlag = 'assets/images/uk_flag.png';
+        _countryName = 'United Kingdom';
+        _cityName = "London";
+        _ipAddr = "192.168.1.2";
+        _provider = "Vodafone";
+      });
+    } else {
+      setState(() {
+        _currentFlag = 'assets/images/flag.png';
+        _countryName = 'Russian Federation';
+        _cityName = "Moscow";
+        _ipAddr = "192.168.1.1";
+        _provider = "MGTS";
+      });
+    }
+
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            padding: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8.0),
+              color: Color(0xFFE6E8EB),
+            ),
+            child: const Row(
+              children: [
+                Icon(Icons.search, color: Colors.grey),
+                SizedBox(width: 10),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: '192.168.1.1',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          Expanded(
+            child: Stack(
+              children: [
+                // Background image
+                Positioned.fill(
+                  child: Opacity(
+                    opacity: 0.3,
+                    child: Image.asset(
+                      'assets/images/world_map.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Spacer(),
+                    Center(
+                      child: Image.asset(
+                        _currentFlag, // Use current flag asset
+                        fit: BoxFit.contain,
+                        height: 150,
+                        width: 225,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      _countryName, // Use current country name
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Spacer(),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          InformationTile(title: 'Страна', value: _countryName),
+          InformationTile(title: 'Город', value: _cityName),
+          InformationTile(title: 'Провайдер', value: _provider),
+          InformationTile(title: 'Подсеть', value: _ipAddr),
+          InformationTile(title: 'Широта', value: '77.245233'),
+          InformationTile(title: 'Долгота', value: '32.234232'),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              Expanded(
+                child: ActionButton(
+                  iconPath: 'assets/icons/show_on_map_ic.svg',
+                  label: 'Показать на карте',
+                  onPressed: () {},
+                ),
+              ),
+              SizedBox(width: 10), // Space between buttons
+              Expanded(
+                child: ActionButton(
+                  iconPath: 'assets/icons/more_data_ic.svg',
+                  label: 'Полные данные',
+                  onPressed: _updateCountryInfo,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
+    );
+  }
+}
+
+class InformationTile extends StatelessWidget {
+  final String title;
+  final String value;
+
+  InformationTile({required this.title, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0),
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: Color(0xFFE6E8EB),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(title, style: TextStyle(fontSize: 16)),
+            Text(value, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ActionButton extends StatelessWidget {
+  final String iconPath;
+  final String label;
+  final VoidCallback onPressed;
+
+  ActionButton({required this.iconPath, required this.label, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Color(0xFFD4D4D4),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+        ),
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Баранов Артем Андреевич',textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text(
-              'ИКБО-12-21', textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text(
-              '21И0401', textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            Text(label,
+                style: TextStyle(fontSize: 16, color: Colors.black)),
+            SizedBox(width: 24),
+            SvgPicture.asset(
+              iconPath,
+              height: 50,
+              width: 50
             )
           ],
         ),
@@ -65,3 +221,5 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+
